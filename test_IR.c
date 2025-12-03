2a564f5b592cc03745b16316103091f4f25d4311
Tejas Vilas Chaudhari <84889817+tejaschaudhari457@users.noreply.github.com>
2025-12-03 15:28:13 +0530

    Update test_IR.c

--- test_IR.c
+++ test_IR.c
@@ -1,19 +1,32 @@
-int irPin = 7;   // IR sensor output pin
+int irPin1 = 8;   // IR Sensor 1 pin  right side 
+int irPin2 = 9;   // IR Sensor 2 pin   left side 
 
 void setup() {
-  pinMode(irPin, INPUT);
+  pinMode(irPin1, INPUT);
+  pinMode(irPin2, INPUT);
+
   Serial.begin(9600);
 }
 
 void loop() {
-  int sensorValue = digitalRead(irPin);
 
-  if (sensorValue == 0) { 
-    // Most IR sensors give LOW when obstacle detected
-    Serial.println("Obstacle Detected!");
+  int sensorValue1 = digitalRead(irPin1);
+  int sensorValue2 = digitalRead(irPin2);
+
+  // ----- Sensor 1 -----
+  if (sensorValue1 == 0) { 
+    Serial.println("IR Sensor 1: Obstacle Detected!");
   } else {
-    Serial.println("No Obstacle");
+    Serial.println("IR Sensor 1: No Obstacle");
   }
 
+  // ----- Sensor 2 -----
+  if (sensorValue2 == 0) { 
+    Serial.println("IR Sensor 2: Obstacle Detected!");
+  } else {
+    Serial.println("IR Sensor 2: No Obstacle");
+  }
+
+  Serial.println("------------------------");
   delay(500);
 }
